7cab6245811b2c6c3bdc0ec002f4ab3fc23b1ddb
Jon <JonCote95@gmail.com>
2024-07-12 20:11:41 -0700

    changed base to directory name

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ const whenExternalScripts = (items = []) =>
 
 export default defineConfig({
   site: 'https://joncote.github.io/',
-  base: 'PortfolioWebsite/',
+  base: '/PortfolioWebsite',
   output: 'static',
 
   integrations: [
